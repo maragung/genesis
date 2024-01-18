0175aecf426b14efbbcf6cfa8eba022a369126e0
maragung <50156713+maragung@users.noreply.github.com>
2024-01-18 23:22:14 +0700

    Create update_massa_mainnet_2.1_x64.sh

---
+++ massa/update_massa_mainnet_2.1_x64.sh
@@ -0,0 +1,27 @@
+# Downloading the Massa cryptocurrency software version 2.1 release for Linux from GitHub.
+echo "Downloading the Massa cryptocurrency software version 2.1 release for Linux from GitHub."
+wget -O massa_MAIN.2.1 https://github.com/massalabs/massa/releases/download/MAIN.2.1/massa_MAIN.2.1_release_linux.tar.gz
+
+# Waiting for the download to complete before proceeding.
+echo "Waiting for the download to complete before proceeding."
+wait
+
+# Stopping the Massa daemon service to perform the update.
+echo "Stopping the Massa daemon service to perform the update."
+sudo systemctl stop massad
+
+# Extracting the contents of the downloaded Massa software archive.
+echo "Extracting the contents of the downloaded Massa software archive."
+tar xvzf massa_MAIN.2.1
+
+# Waiting for the extraction process to finish.
+echo "Waiting for the extraction process to finish."
+wait
+
+# Starting the Massa daemon service after the update.
+echo "Starting the Massa daemon service after the update."
+sudo systemctl start massad
+
+# Removing the downloaded Massa software archive to clean up.
+echo "Removing the downloaded Massa software archive to clean up."
+rm massa_MAIN.2.1
